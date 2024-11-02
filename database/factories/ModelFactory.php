9ea029ca847b5ba5409e97d355c0ec062228c816
tok3 <tobias@eq3w.de>
2024-11-02 11:11:31 +0100

    update

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Tok3\Example\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,5 @@
 <?php
 
-use VendorName\Skeleton\Tests\TestCase;
+use Tok3\Example\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
